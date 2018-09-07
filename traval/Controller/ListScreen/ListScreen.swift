9173bea8800f017abef2ced502725a492543ac7b
kotecaava <kote.caava@gmail.com>
2018-09-07 15:19:52 +0400

    First upload trav app

---
+++ traval/Controller/ListScreen/ListScreen.swift
@@ -0,0 +1,23 @@
+//
+//  ListScreen.swift
+//  traval
+//
+//  Created by kote caava on 9/7/18.
+//  Copyright © 2018 didicomp. All rights reserved.
+//
+
+import UIKit
+
+class ListScreen: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
